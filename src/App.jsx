8cb64640da48d1f4c03890b4b19323cb8508aa97
Victor-Okpukpan <vokpukpan@gmail.com>
2023-04-25 08:27:37 +0100

    Performed code splitting with react.lazy() to optimize performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
-import SideBar from './components/SideBar';
-import Home from './components/Home';
-import Projects from './components/Projects';
-import StarsCanvas from './components/StarsCanvas';
-import Tech from './components/Tech';
-import Contact from './components/Contact';
-import Experience from './components/Experience';
-import Footer from './components/Footer';
+import { lazy, Suspense } from 'react';
+const SideBarComponent = lazy(() => import('./components/SideBar'));
+const HomeComponent = lazy(() => import('./components/Home'));
+const ProjectsComponent = lazy(() => import('./components/Projects'));
+const StarsCanvasComponent = lazy(() => import('./components/StarsCanvas'));
+const ContactComponent = lazy(() => import('./components/Contact'));
+const ExperienceComponent = lazy(() => import('./components/Experience'));
+const FooterComponent = lazy(() => import('./components/Footer'));
+const TechComponent = lazy(() => import('./components/Tech'));
 import { BrowserRouter } from 'react-router-dom';
+const renderLoader = () => {
+  return (
+    <section id="loader" className="w-full h-screen bg-black text-purple-600 flex items-center justify-center">
+      <div>
+        <p className='text-3xl md:text-4xl font-bold'>Loading...</p>
+      </div>
+    </section>
+  )
+}
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Suspense fallback={renderLoader()}>
       <div className='relative z-0 font-MyFont bg-black tracking-wider'>
-        <SideBar />
-        <Home />
-        <Experience />
-        <Tech />
-        <StarsCanvas />
-        <Projects />
-        <Contact />
-        <Footer />
+        <SideBarComponent />
+        <HomeComponent />
+        <ExperienceComponent />
+        <TechComponent />
+        <StarsCanvasComponent />
+        <ProjectsComponent />
+        <ContactComponent />
+        <FooterComponent />
       </div>
+    </Suspense>
     </BrowserRouter>
   )
 }
